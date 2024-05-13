9c43f1709bc6f4ca257f91fc60d631cee2e4b841
Anna <72981644+anna-rosew@users.noreply.github.com>
2024-05-13 17:19:23 +0100

    Integrated the API within the InnerHTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generateRecipe (event){
-    event.preventDefault();
+function displayRecipe(response) {
     new Typewriter('#recipe', {
-        strings: "This is how to make a Tom collins",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
       });
+}
+
+function generateRecipe (event){
+    event.preventDefault();
+
+    let recipeInput= document.querySelector("#recipe-input");
+
+      let prompt= `Generate a recipe for a ${recipeInput.value} cocktail`
+      let context = "You are an experienced bartender who's worked at bars around the world and won awards for your drinks. Generate an easy to follow recipe with a <section> with a list with the class of ingredients of the ingredients. Then a <br/> to seperate it from another another <section> with a list who's class is method, this should contain a numbered list of steps for making the cocktail. Each section should have a title in <h3>. Include <h4>Enjoy your drink!<h4>at the bottom centred. The title of the drink should be in h2 at the top"
+      let apiKey= "a53b648o522cd05fa66cde020a59ta02"
+      let apiUrl= `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+      axios.get(apiUrl).then(displayRecipe);
 
 }
 
 let recipeFormElement = document.querySelector("#recipe-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
+
+//Build some suggestions?
