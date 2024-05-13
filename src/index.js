c03564d0e0642f29dcad9dea911e9bf2fb95a135
Anna <72981644+anna-rosew@users.noreply.github.com>
2024-05-13 17:50:32 +0100

    Loading image

--- src/index.js
+++ src/index.js
@@ -12,9 +12,12 @@ function generateRecipe (event){
     let recipeInput= document.querySelector("#recipe-input");
 
       let prompt= `Generate a recipe for a ${recipeInput.value} cocktail`
-      let context = "You are an experienced bartender who's worked at bars around the world and won awards for your drinks. Generate an easy to follow recipe with a <section> with a list with the class of ingredients of the ingredients. Then a <br/> to seperate it from another another <section> with a list who's class is method, this should contain a numbered list of steps for making the cocktail. Each section should have a title in <h3>. Include <h4>Enjoy your drink!<h4>at the bottom centred. The title of the drink should be in h2 at the top"
+      let context = "You are an experienced bartender who's worked at bars around the world and won awards for your drinks. Generate an easy to follow recipe with a <section> with a list with the class of ingredients of the ingredients. Then a <br/> to seperate it from another another <section> with a list who's class is method, this should contain a numbered list of steps for making the cocktail. Each section should have a title in <h3>. Include a centred strong Enjoy your drink! at the bottom. The title of the drink should be in h2 at the top"
       let apiKey= "a53b648o522cd05fa66cde020a59ta02"
       let apiUrl= `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+      let recipeELement= document.querySelector("#recipe");
+      recipeELement.innerHTML = `<h4>Generating a delicious recipe for ${recipeInput.value}...hold tight!</h4><img src="src/imgs/giphy.gif" alt="Dog shaking cocktail">`
 
       axios.get(apiUrl).then(displayRecipe);
 
